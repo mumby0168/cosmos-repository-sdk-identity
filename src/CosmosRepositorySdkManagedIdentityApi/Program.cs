efdf9194ab74e3ffa69e6c59862102a755008595
Billy Mumby <billy.mumby@outlook.com>
2022-05-23 20:30:01 +0100

    Update example to use bogus to generate fake data.

--- src/CosmosRepositorySdkManagedIdentityApi/Program.cs
+++ src/CosmosRepositorySdkManagedIdentityApi/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Identity;
+using Bogus;
 using Microsoft.Azure.CosmosRepository;
 using Microsoft.Azure.CosmosRepository.Options;
 using Microsoft.Extensions.Options;
@@ -18,17 +19,31 @@ var app = builder.Build();
 app.MapGet("/", () => "Cosmos Repository SDK - Managed Identity Demo");
 
 app.MapGet("/books", async (IRepository<Book> repository) =>
-    await repository.GetAsync(x => x.Type == nameof(Book)));
+    await repository.GetAsync(x => x.PartitionKey == nameof(Book)));
 
-app.MapGet("/config", (IOptionsMonitor<RepositoryOptions> options) =>
-    new
-    {
-        dns = options.CurrentValue.AccountEndpoint,
-        db = options.CurrentValue.DatabaseId
-    });
+app.MapGet("/seed", async (IWriteOnlyRepository<Book> repository) =>
+{
+    Faker<Book> booksFaker = new();
+    booksFaker
+        .RuleFor(p => p.Name, f => f.Company.CatchPhrase());
+
+    List<Book> books = booksFaker.Generate(100);
+    await repository.CreateAsync(books);
+});
 
 app.Run();
 
 public class Book : EtagItem
 {
+    public string Name { get; set; } = null!;
+
+    public string PartitionKey { get; set; }
+
+    protected override string GetPartitionKeyValue() =>
+        PartitionKey;
+
+    public Book()
+    {
+        PartitionKey = nameof(Book);
+    }
 }
